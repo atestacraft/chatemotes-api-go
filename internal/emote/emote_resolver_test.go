9193c4de892192de442fc14ffc33f8cd1ef40060
Poletansky Viktor <poletansky@gmail.com>
2023-03-14 13:31:34 +0300

    master: u

--- internal/emote/emote_resolver_test.go
+++ internal/emote/emote_resolver_test.go
@@ -8,37 +8,31 @@ import (
 )
 
 func TestSevenTv(t *testing.T) {
-	assert := assert.New(t)
-
 	url1, ok := emote_resolver.EmoteResolver.ResolveUrl("https://7tv.app/emotes/60ae958e229664e8667aea38")
-	assert.True(ok)
-	assert.Equal(url1, "https://cdn.7tv.app/emote/60ae958e229664e8667aea38/2x.webp")
+	assert.True(t, ok)
+	assert.Equal(t, url1, "https://cdn.7tv.app/emote/60ae958e229664e8667aea38/2x.webp")
 
 	url2, ok := emote_resolver.EmoteResolver.ResolveUrl("https://cdn.7tv.app/emote/60ae958e229664e8667aea38/4x.webp")
-	assert.True(ok)
-	assert.Equal(url2, "https://cdn.7tv.app/emote/60ae958e229664e8667aea38/2x.webp")
+	assert.True(t, ok)
+	assert.Equal(t, url2, "https://cdn.7tv.app/emote/60ae958e229664e8667aea38/2x.webp")
 }
 
 func TestBetterTv(t *testing.T) {
-	assert := assert.New(t)
+	url1, ok := emote_resolver.EmoteResolver.ResolveUrl("https://betterttv.com/emotes/5f1b0186cf6d2144653d2970")
+	assert.True(t, ok)
+	assert.Equal(t, url1, "https://cdn.betterttv.net/emote/5f1b0186cf6d2144653d2970/2x")
 
-	url1, err := emote_resolver.EmoteResolver.ResolveUrl("https://betterttv.com/emotes/5f1b0186cf6d2144653d2970")
-	assert.True(err)
-	assert.Equal(url1, "https://cdn.betterttv.net/emote/5f1b0186cf6d2144653d2970/2x")
-
-	url2, err := emote_resolver.EmoteResolver.ResolveUrl("https://cdn.betterttv.net/emote/5f1b0186cf6d2144653d2970/3x.webp")
-	assert.True(err)
-	assert.Equal(url2, "https://cdn.betterttv.net/emote/5f1b0186cf6d2144653d2970/2x")
+	url2, ok := emote_resolver.EmoteResolver.ResolveUrl("https://cdn.betterttv.net/emote/5f1b0186cf6d2144653d2970/3x.webp")
+	assert.True(t, ok)
+	assert.Equal(t, url2, "https://cdn.betterttv.net/emote/5f1b0186cf6d2144653d2970/2x")
 }
 
 func TestFrankerFaceZ(t *testing.T) {
-	assert := assert.New(t)
+	url1, ok := emote_resolver.EmoteResolver.ResolveUrl("https://www.frankerfacez.com/emoticon/128054-OMEGALUL")
+	assert.True(t, ok)
+	assert.Equal(t, url1, "https://cdn.frankerfacez.com/emoticon/128054/2")
 
-	url1, err := emote_resolver.EmoteResolver.ResolveUrl("https://www.frankerfacez.com/emoticon/128054-OMEGALUL")
-	assert.True(err)
-	assert.Equal(url1, "https://cdn.frankerfacez.com/emoticon/128054/2")
-
-	url2, err := emote_resolver.EmoteResolver.ResolveUrl("https://cdn.frankerfacez.com/emoticon/128054/3")
-	assert.True(err)
-	assert.Equal(url2, "https://cdn.frankerfacez.com/emoticon/128054/2")
+	url2, ok := emote_resolver.EmoteResolver.ResolveUrl("https://cdn.frankerfacez.com/emoticon/128054/3")
+	assert.True(t, ok)
+	assert.Equal(t, url2, "https://cdn.frankerfacez.com/emoticon/128054/2")
 }
