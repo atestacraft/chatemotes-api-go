6fc3152d9c439c518f0301ecaa57fd560e8f0346
Poletansky Viktor <poletansky@gmail.com>
2023-03-14 13:21:56 +0300

    master: u

--- internal/emote/emote_resolver.go
+++ internal/emote/emote_resolver.go
@@ -11,10 +11,6 @@ type emoteResolver struct {
 	imgFmt string
 }
 
-type Resolver struct {
-	resolvers []emoteResolver
-}
-
 func (r emoteResolver) resolve(url string) (string, bool) {
 	matches := r.regex.FindStringSubmatch(url)
 
@@ -25,46 +21,46 @@ func (r emoteResolver) resolve(url string) (string, bool) {
 	return fmt.Sprintf(r.imgFmt, matches[r.index]), true
 }
 
-var EmoteResolver = newEmoteResolver()
+type Resolver struct {
+	resolvers []emoteResolver
+}
 
-func newEmoteResolver() *Resolver {
-	return &Resolver{
-		resolvers: []emoteResolver{
-			// 7tv
-			{
-				regex:  regexp.MustCompile(`^https://7tv.app/emotes/(\w+)$`),
-				index:  1,
-				imgFmt: "https://cdn.7tv.app/emote/%s/2x.webp",
-			},
-			{
-				regex:  regexp.MustCompile(`^https://cdn.7tv.app/emote/(\w+)`),
-				index:  0,
-				imgFmt: "%s/2x.webp",
-			},
-			// bttv
-			{
-				regex:  regexp.MustCompile(`^https:\/\/betterttv.com\/emotes\/(\w+)`),
-				index:  1,
-				imgFmt: "https://cdn.betterttv.net/emote/%s/2x",
-			},
-			{
-				regex:  regexp.MustCompile(`^https:\/\/cdn.betterttv.net\/emote\/(\w+)`),
-				index:  0,
-				imgFmt: "%s/2x",
-			},
-			// ffz
-			{
-				regex:  regexp.MustCompile(`^https:\/\/www.frankerfacez.com\/emoticon\/(\d+)`),
-				index:  1,
-				imgFmt: "https://cdn.frankerfacez.com/emoticon/%s/2",
-			},
-			{
-				regex:  regexp.MustCompile(`^https:\/\/cdn.frankerfacez.com\/emoticon\/(\w+)`),
-				index:  0,
-				imgFmt: "%s/2",
-			},
+var EmoteResolver = &Resolver{
+	resolvers: []emoteResolver{
+		// 7tv
+		{
+			regex:  regexp.MustCompile(`^https://7tv.app/emotes/(\w+)$`),
+			index:  1,
+			imgFmt: "https://cdn.7tv.app/emote/%s/2x.webp",
 		},
-	}
+		{
+			regex:  regexp.MustCompile(`^https://cdn.7tv.app/emote/(\w+)`),
+			index:  0,
+			imgFmt: "%s/2x.webp",
+		},
+		// bttv
+		{
+			regex:  regexp.MustCompile(`^https:\/\/betterttv.com\/emotes\/(\w+)`),
+			index:  1,
+			imgFmt: "https://cdn.betterttv.net/emote/%s/2x",
+		},
+		{
+			regex:  regexp.MustCompile(`^https:\/\/cdn.betterttv.net\/emote\/(\w+)`),
+			index:  0,
+			imgFmt: "%s/2x",
+		},
+		// ffz
+		{
+			regex:  regexp.MustCompile(`^https:\/\/www.frankerfacez.com\/emoticon\/(\d+)`),
+			index:  1,
+			imgFmt: "https://cdn.frankerfacez.com/emoticon/%s/2",
+		},
+		{
+			regex:  regexp.MustCompile(`^https:\/\/cdn.frankerfacez.com\/emoticon\/(\w+)`),
+			index:  0,
+			imgFmt: "%s/2",
+		},
+	},
 }
 
 func (r *Resolver) ResolveUrl(url string) (string, bool) {
